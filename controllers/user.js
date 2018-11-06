a828f7da00111f83ecbf8f21e32ec5bdca324d62
daffadwiputra <daffaputradamar@gmail.com>
2018-11-06 21:15:03 +0700

    adding search functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,46 +1,50 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
 
 function createHash(password) {
   return new Promise(async (resolve, reject) => {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    resolve(hash);
-  });
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    resolve(hash)
+  })
 }
 
 function isPasswordMatch(hashedPassword, password) {
   return new Promise(async (resolve, reject) => {
-    isMatch = await bcrypt.compare(hashedPassword, password);
-    resolve(isMatch);
-  });
+    isMatch = await bcrypt.compare(hashedPassword, password)
+    resolve(isMatch)
+  })
 }
 
 module.exports = {
   index: async (req, res) => {
-    res.json(await User.find());
+    res.json(await User.find())
   },
   show: async (req, res) => {
-    res.json(await User.findById(req.params._id));
+    res.json(await User.findById(req.params._id))
   },
   store: async (req, res) => {
-    let { user } = req.body;
-    user.password = await createHash(user.password);
-    res.json(await User.create(user));
+    let { user } = req.body
+    user.password = await createHash(user.password)
+    res.json(await User.create(user))
   },
   update: async (req, res) => {
-    const { user } = req.body;
-    const { _id } = req.params;
-    res.json(await User.findOneAndUpdate({ _id }, { $set: user }));
+    const { user } = req.body
+    const { _id } = req.params
+    res.json(await User.findOneAndUpdate({ _id }, { $set: user }))
   },
   destroy: async (req, res) => {
-    res.json(await User.findOneAndDelete({ _id: req.params._id }));
+    res.json(await User.findOneAndDelete({ _id: req.params._id }))
   },
   authenticate: async (req, res) => {
-    let { user } = req.body;
-    userFromDb = await User.findOne({ username: user.username });
+    let { user } = req.body
+    userFromDb = await User.findOne({ username: user.username })
     res.json(
       (await isPasswordMatch(userFromDb.password, user.password)) ? true : false
-    );
+    )
+  },
+  search: async (req, res) => {
+    const { skill } = req.body
+    res.json(await User.find({ skills: skill }))
   }
-};
+}
